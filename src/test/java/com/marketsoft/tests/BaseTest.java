31bef11d4f6658296c2edeedf150e67b7e4aa421
Unknown <miskhat.sheikh@gmail.com>
2018-04-07 17:51:15 +0200

    Cross browser test
    Supports running the same tests for multiple browsers.

--- src/test/java/com/marketsoft/tests/BaseTest.java
+++ src/test/java/com/marketsoft/tests/BaseTest.java
@@ -3,11 +3,18 @@ package com.marketsoft.tests;
 import com.marketsoft.utilities.PageCreator;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.opera.OperaDriver;
+import org.openqa.selenium.opera.OperaOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterMethod;
+
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+
+import java.io.File;
 
 public class BaseTest {
 
@@ -18,11 +25,37 @@ public class BaseTest {
     final int timeout = 10;
 
     @BeforeClass
-    public void setUpBeforeClass() {
+    @Parameters("browser")
+    public void setUpBeforeClass(String browser) {
 
-        System.setProperty("webdriver.chrome.driver", "chromedriver");
+        switch (browser){
+            case "chrome":
+                System.setProperty("webdriver.chrome.driver", "chromedriver");
+                testDriver = new ChromeDriver();
+                break;
+            case "firefox":
+                System.setProperty("webdriver.gecko.driver", "geckodriver");
+                testDriver = new FirefoxDriver();
+                break;
+            case "opera":
+                OperaOptions options = new OperaOptions();
 
-        testDriver = new ChromeDriver();
+                options.setBinary(new File("operadriver"));
+
+
+//                System.setProperty("webdriver.chrome.driver","operadriver");
+                testDriver = new OperaDriver(options);
+
+                break;
+            default:
+                System.setProperty("webdriver.chrome.driver","chromedriver");
+                testDriver = new ChromeDriver();
+                break;
+        }
+
+//        System.setProperty("webdriver.chrome.driver", "chromedriver");
+//
+//        testDriver = new ChromeDriver();
 
         wait = new WebDriverWait(testDriver,timeout);
 
@@ -32,11 +65,15 @@ public class BaseTest {
 
     @BeforeMethod
     public void setUpBeforeMethod(){
+
         testPage = new PageCreator(testDriver);
     }
 
     @AfterClass
     public void tearDown(){
+
+        testDriver.close();
+
         testDriver.quit();
     }
 
